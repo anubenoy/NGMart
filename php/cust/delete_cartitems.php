204f44b0a42924b09d05a4aec1b2adbbb7c72f45
anubenoy <anubenoy@mca.ajce.in>
2021-06-12 14:46:24 +0530

    order plcaement set

--- php/cust/delete_cartitems.php
+++ php/cust/delete_cartitems.php
@@ -8,9 +8,21 @@ include("../dbconnection.php");
  if(isset($_GET['id']) ){
     $cart_id=$_GET["id"];
     $sql="DELETE FROM cart_tbl WHERE cart_id=$cart_id";
-    if($result=mysqli_query($con,$sql)){
-    header("location:cart.php");
+    if($result=mysqli_query($con,$sql))
+    {
+        //check if redirection from cart pg or order summary pg
+        if(isset($_GET['order_final']))
+        {
+            if(isset($_GET['add_id']))
+            {
+                $add_id=$_GET['add_id']; echo $add_id;
+                header("location:order.php?add=$add_id");  
+            }
+        }
+        else{ header("location:cart.php"); }
+                    
     }
+    
 }
 
 ?>
